6811d0a9aa1ccaf8bc1082709d16e1138437a871
jakub Przywoski <jprzywoski@tlen.pl>
2020-02-17 09:42:17 +0000

    first commit

---
+++ Makefile
@@ -0,0 +1,159 @@
+# Notes
+#
+# TARGETS
+#
+# The default TARGET is set to DEVEL with all warnings cranked up to maximum.
+# This should be the mode used during development / debug cycle.
+#
+# RELEASE is for the final binary release, it is also a good practice to strip
+# the resulting binaries using: 'strip --strip-unneeded <binary>'
+#
+# Profiled builds are a bit trickier, one has to compile twice:
+# 1. first compile with TARGET := $(GENERATE_PROFILE) and -lgcov linker flag
+# 2. run your application to collect peformance samples (.gcda files in the
+# build directory)
+# 3. run make clean to delete old .o object files
+# 4. now run make again with TARGET := $(PROFILED_RELEASE) to compile a new
+# binary using .gcda files for extra level of optimization
+# Warning! The default flags -march=native and -march=tune will likely make the
+# binary non backwards compatible CPU-wise, but this will squeeze out the best
+# performance for the particular machine we are building on.
+#
+# Static Code Analysis
+# this will require 'flawfinder' and 'cppcheck' tools:
+# sudo apt install flawfinder cppcheck
+# now run 'make static-analysis' rule to make sure the code is squeaky clean.
+
+#
+# name of the resulting binary file
+#
+BUILD_ARTIFACT := contro
+
+SHELL := /usr/bin/env bash
+CC := gcc
+
+#
+# color output
+#
+PRINT = @echo -e "\e[1;32mBuilding $<\e[0m"
+
+#
+# TARGET can be only one of the below
+#
+DEVEL := 1
+RELEASE := 2
+GENERATE_PROFILE := 3
+PROFILED_RELEASE := 4
+
+#
+# select one only at a time
+#
+TARGET := $(DEVEL)
+# TARGET := $(RELEASE)
+# TARGET := $(GENERATE_PROFILE)
+# TARGET := $(PROFILED_RELEASE)
+
+#
+# -I, -D preprocessor options
+#
+CPPFLAGS = 
+ifneq ($(TARGET), $(DEVEL))
+	CPPFLAGS += -DNDEBUG
+endif
+
+#
+# debugging and optimization options for the C and C++ compilers
+#
+ifeq ($(TARGET), $(DEVEL))
+
+	# basic warnings
+	CFLAGS := -std=c90 -pedantic -Og -g3 -Wall -pipe
+	CFLAGS += -Werror
+
+    # extended warning and debug options
+	CFLAGS += -Wlogical-op -Wdouble-promotion -Wshadow -Wformat=2
+	CFLAGS += -Wcast-align -Winvalid-pch -Wmissing-include-dirs
+	CFLAGS += -Wredundant-decls -Wswitch-default -Wswitch-enum
+	CFLAGS += -Wmissing-prototypes -Wconversion -Wcast-qual
+	CFLAGS += -Wduplicated-cond -Wduplicated-branches -Wrestrict
+	CFLAGS += -Wnull-dereference -Wjump-misses-init
+	CFLAGS += -Wmissing-declarations -Wodr 
+
+	# debugger experience
+	CFLAGS += -Og -g3 -fno-omit-frame-pointer -fno-strict-aliasing
+	CFLAGS += -fno-optimize-sibling-calls -fasynchronous-unwind-tables
+	CFLAGS += -fexceptions
+
+    # security options
+	CPPFLAGS += -D_FORTIFY_SOURCE=2
+	CFLAGS += -fstack-protector-strong
+	CFLAGS += -fPIE -pie -Wl,-pie -Wl,-z,defs -Wl,-z,now -Wl,-z,relro
+
+    # sanitizers, some require clang
+	CFLAGS += -fsanitize=address
+	CFLAGS += -fsanitize=leak
+	# CFLAGS += -fsanitize=address -fsanitize=pointer-compare
+	# CFLAGS += -fsanitize=pointer-subtract
+	# run with: ASAN_OPTIONS=detect_invalid_pointer_pairs=2: ./a.out
+	CFLAGS += -fsanitize=undefined
+	# CFLAGS += -fsanitize=thread
+	# CFLAGS += -fsanitize=memory
+    
+else ifeq ($(TARGET), $(RELEASE))
+	CFLAGS := -O2
+	# CFLAGS += -fopt-info-vec-all
+	# CFLAGS += -Q --help=target --help=optimizers
+else ifeq ($(TARGET), $(GENERATE_PROFILE))
+	CFLAGS := -Ofast -march=native -mtune=native -flto -fprofile-generate -pipe
+else ifeq ($(TARGET), $(PROFILED_RELEASE))
+	CFLAGS := -Ofast -march=native -mtune=native -flto -fprofile-use -pipe
+	# CFLAGS += -fprofile-correction
+endif
+
+#
+# -L options for the linker
+#
+LDFLAGS := -L.
+
+#
+# -l options to pass to the linker
+#
+LDLIBS := -lSDL2 -lSDL2_image
+ifeq ($(TARGET), $(DEVEL))
+	LDLIBS += -fsanitize=address -static-libasan
+	LDLIBS += -fsanitize=undefined -static-libubsan
+endif
+ifeq ($(TARGET),$(filter $(TARGET),$(GENERATE_PROFILE) $(PROFILED_RELEASE)))
+	LDLIBS += -lgcov
+endif
+
+#
+# linking
+#
+OBJECTS := main.o
+all: $(OBJECTS)
+	$(CC) $(CPPFLAGS) $(CFLAGS) $(OBJECTS) $(LDFLAGS) $(LDLIBS) -o $(BUILD_ARTIFACT)
+
+#
+# compiling
+#
+main.o: main.c
+	$(PRINT)
+	$(CC) $(CPPFLAGS) $(CFLAGS) -c $< $(LDFLAGS) $(LDLIBS)
+
+clean:
+	rm -f *.o $(BUILD_ARTIFACT)
+	rm -rf "./infer-out"
+
+very-clean:
+	rm -f *.o *.gcda $(BUILD_ARTIFACT)
+
+static-analysis:
+	@echo -e "\e[1;33mAnalazying: cppcheck... \e[0m"
+	cppcheck $(CPPFLAGS) std=89 *.c *.h
+	@echo -e "\e[1;33mAnalazying: infer... \e[0m"	
+	infer run -- gcc -c main.c
+	@echo -e "\e[1;33mAnalazying: flawfinder... \e[0m"	
+	flawfinder main.c
+	@echo -e "\e[1;33mAnalazying: splint... \e[0m"	
+	splint --weak $(CPPFLAGS) main.c
---
+++ main.c
@@ -0,0 +1,370 @@
+
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <stdio.h>
+
+#define MAX_BULLETS 1000
+
+typedef struct {
+  float x, y, dy;
+  short life;
+  char *name;
+  int currentSprite, walking, facingLeft, shooting, visible;
+  int alive;
+
+  SDL_Texture *sheetTexture;
+} Man;
+
+typedef struct {
+  float x, y, dx;
+} Bullet;
+
+SDL_Texture *bulletTexture;
+SDL_Texture *backgroundTexture;
+Bullet *bullets[MAX_BULLETS] = {0, };
+Man enemy;
+
+int globalTime = 0;
+
+void addBullet(float x, float y, float dx);
+void removeBullet(int i);
+int processEvents(SDL_Window *window, Man *man);
+void doRender(SDL_Renderer *renderer, Man *man);
+void updateLogic(Man *man);
+
+void addBullet(float x, float y, float dx) {
+  int found = -1;
+  int i;
+  for (i = 0; i < MAX_BULLETS; i++) {
+    if (bullets[i] == NULL) {
+      found = i;
+      break;
+    }
+  }
+
+  if (found >= 0) {
+    i = found;
+    bullets[i] = malloc(sizeof(Bullet));
+    bullets[i]->x = x;
+    bullets[i]->y = y;
+    bullets[i]->dx = dx;
+  }
+}
+
+void removeBullet(int i) {
+  if (bullets[i]) {
+    free(bullets[i]);
+    bullets[i] = NULL;
+  }
+}
+
+int processEvents(SDL_Window *window, Man *man) {
+  SDL_Event event;
+  int done = 0;
+  const Uint8 *state;
+
+  while (SDL_PollEvent(&event)) {
+    switch (event.type) {
+    case SDL_WINDOWEVENT_CLOSE: {
+      if (window) {
+        SDL_DestroyWindow(window);
+        window = NULL;
+        done = 1;
+      }
+    } break;
+    case SDL_KEYDOWN: {
+      switch (event.key.keysym.sym) {
+      case SDLK_ESCAPE:
+        done = 1;
+        break;
+      default:
+        break;
+      }
+    } break;
+    case SDL_QUIT:
+      /* quit out of the game */
+      done = 1;
+      break;
+    default:
+      break;
+    }
+  }
+
+  state = SDL_GetKeyboardState(NULL);
+  if (!man->shooting) {
+    if (state[SDL_SCANCODE_LEFT]) {
+      man->x -= 3;
+      man->walking = 1;
+      man->facingLeft = 1;
+
+      if (globalTime % 6 == 0) {
+        man->currentSprite++;
+        man->currentSprite %= 4;
+      }
+    } else if (state[SDL_SCANCODE_RIGHT]) {
+      man->x += 3;
+      man->walking = 1;
+      man->facingLeft = 0;
+
+      if (globalTime % 6 == 0) {
+        man->currentSprite++;
+        man->currentSprite %= 4;
+      }
+    } else {
+      man->walking = 0;
+      man->currentSprite = 4;
+    }
+  }
+
+  if (!man->walking) {
+    if (state[SDL_SCANCODE_SPACE]) /* && !man->dy) */
+    {
+      if (globalTime % 6 == 0) {
+        if (man->currentSprite == 4)
+          man->currentSprite = 5;
+        else
+          man->currentSprite = 4;
+
+        if (!man->facingLeft) {
+          addBullet(man->x + 35, man->y + 20, 3);
+        } else {
+          addBullet(man->x + 5, man->y + 20, -3);
+        }
+      }
+
+      man->shooting = 1;
+    } else {
+      man->currentSprite = 4;
+      man->shooting = 0;
+    }
+  }
+
+  if (state[SDL_SCANCODE_UP] && !man->dy) {
+    man->dy = -8;
+  }
+  if (state[SDL_SCANCODE_DOWN]) {
+    /* man->y += 10; */
+  }
+
+  return done;
+}
+
+void doRender(SDL_Renderer *renderer, Man *man) {
+  int i;
+  /* set the drawing color to blue */
+  SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
+
+  /* Clear the screen (to blue) */
+  SDL_RenderClear(renderer);
+
+  /* set the drawing color to white */
+  SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+
+  /* SDL_RenderFillRect(renderer, &rect); */
+  SDL_RenderCopy(renderer, backgroundTexture, NULL, NULL);
+
+  /* warrior */
+  if (man->visible) {
+    SDL_Rect srcRect;
+    SDL_Rect rect;
+
+    srcRect.x = 40 * man->currentSprite;
+    srcRect.y = 0;
+    srcRect.w = 40;
+    srcRect.h = 50;
+
+    rect.x = (int)man->x;
+    rect.y = (int)man->y;
+    rect.w = 40;
+    rect.h = 50;
+    SDL_RenderCopyEx(renderer, man->sheetTexture, &srcRect, &rect, 0, NULL,
+                     man->facingLeft);
+  }
+
+  /* enemy */
+  if (enemy.visible) {
+    SDL_Rect eSrcRect;
+    SDL_Rect eRect;
+
+    eSrcRect.x = 40 * 40 * enemy.currentSprite;
+    eSrcRect.y = 0;
+    eSrcRect.w = 40;
+    eSrcRect.h = 50;
+
+    eRect.x = (int)enemy.x;
+    eRect.y = (int)enemy.y;
+    eRect.w = 40;
+    eRect.h = 50;
+
+    SDL_RenderCopyEx(renderer, enemy.sheetTexture, &eSrcRect, &eRect, 0, NULL,
+                     enemy.facingLeft);
+  }
+
+  for (i = 0; i < MAX_BULLETS; i++)
+    if (bullets[i]) {
+      SDL_Rect rect;
+
+      rect.x = (int)bullets[i]->x;
+      rect.y = (int)bullets[i]->y;
+      rect.w = 8;
+      rect.h = 8;
+
+      SDL_RenderCopy(renderer, bulletTexture, NULL, &rect);
+    }
+
+  /* We are done drawing, "present" or show to the screen what we've drawn */
+  SDL_RenderPresent(renderer);
+}
+
+void updateLogic(Man *man) {
+  int i;
+  man->y += man->dy;
+  man->dy += 0.5f;
+  if (man->y > 60) {
+    man->y = 60;
+    man->dy = 0;
+  }
+
+  for (i = 0; i < MAX_BULLETS; i++)
+    if (bullets[i]) {
+      bullets[i]->x += bullets[i]->dx;
+
+      /* simple coll. detection */
+      if (bullets[i]->x > enemy.x && bullets[i]->x < enemy.x + 40 &&
+          bullets[i]->y > enemy.y && bullets[i]->y < enemy.y + 50) {
+        enemy.alive = 0;
+      }
+
+      if (bullets[i]->x < -1000 || bullets[i]->x > 1000)
+        removeBullet(i);
+    }
+
+  if (enemy.alive == 0 && globalTime % 6 == 0) {
+    if (enemy.currentSprite < 6)
+      enemy.currentSprite = 6;
+    else if (enemy.currentSprite >= 6) {
+      enemy.currentSprite++;
+      if (enemy.currentSprite > 7) {
+        enemy.visible = 0;
+        enemy.currentSprite = 7;
+      }
+    }
+  }
+
+  globalTime++;
+}
+
+int main(void) {
+  Man man;
+  SDL_Window *window;     /* Declare a window */
+  SDL_Renderer *renderer; /* Declare a renderer */
+  SDL_Surface *sheet;
+  SDL_Surface *bg;
+  SDL_Surface *bullet;
+  int done;
+  int i;
+
+  SDL_Init(SDL_INIT_VIDEO); /* Initialize SDL2 */
+
+  man.x = 50;
+  man.y = 0;
+  man.currentSprite = 4;
+  man.alive = 1;
+  man.visible = 1;
+  man.facingLeft = 0;
+
+  enemy.x = 250;
+  enemy.y = 60;
+  enemy.currentSprite = 4;
+  enemy.facingLeft = 1;
+  enemy.alive = 1;
+  enemy.visible = 1;
+
+  /* Create an application window with the following settings: */
+  window = SDL_CreateWindow("Game Window",           /* window title */
+                            SDL_WINDOWPOS_UNDEFINED, /* initial x position */
+                            SDL_WINDOWPOS_UNDEFINED, /* initial y position */
+                            640,                     /* width, in pixels */
+                            480,                     /* height, in pixels */
+                            0                        /* flags */
+  );
+  renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+  SDL_RenderSetLogicalSize(renderer, 320, 240);
+
+  sheet = IMG_Load("sheet.png");
+  if (!sheet) {
+    printf("Cannot find sheet\n");
+    puts(IMG_GetError());
+    return 1;
+  }
+
+  man.sheetTexture = SDL_CreateTextureFromSurface(renderer, sheet);
+  SDL_FreeSurface(sheet);
+
+  /* load enemy */
+  sheet = IMG_Load("badman_sheet.png");
+  if (!sheet) {
+    printf("Cannot find enemy sheet\n");
+    return 1;
+  }
+
+  enemy.sheetTexture = SDL_CreateTextureFromSurface(renderer, sheet);
+  SDL_FreeSurface(sheet);
+
+  /* load the bg */
+  bg = IMG_Load("background.png");
+
+  if (!sheet) {
+    printf("Cannot find background\n");
+    return 1;
+  }
+
+  backgroundTexture = SDL_CreateTextureFromSurface(renderer, bg);
+  SDL_FreeSurface(bg);
+
+  /* load the bullet */
+  bullet = IMG_Load("bullet.png");
+
+  if (!bullet) {
+    printf("Cannot find bullet\n");
+    return 1;
+  }
+
+  bulletTexture = SDL_CreateTextureFromSurface(renderer, bullet);
+  SDL_FreeSurface(bullet);
+
+  /* The window is open: enter program loop (see SDL_PollEvent) */
+  done = 0;
+
+  /* Event loop */
+  while (!done) {
+    /* Check for events */
+    done = processEvents(window, &man);
+
+    /* Update logic */
+    updateLogic(&man);
+
+    /* Render display */
+    doRender(renderer, &man);
+
+    /* don't burn up the CPU */
+    SDL_Delay(10);
+  }
+
+  /* Close and destroy the window */
+  SDL_DestroyWindow(window);
+  SDL_DestroyRenderer(renderer);
+  SDL_DestroyTexture(man.sheetTexture);
+  SDL_DestroyTexture(backgroundTexture);
+  SDL_DestroyTexture(bulletTexture);
+  SDL_DestroyTexture(enemy.sheetTexture);
+
+  for (i = 0; i < MAX_BULLETS; i++)
+    removeBullet(i);
+
+  /* Clean up */
+  SDL_Quit();
+  return 0;
+}
